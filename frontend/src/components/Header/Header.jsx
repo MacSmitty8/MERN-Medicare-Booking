15d46d69a6034cfdc6cbfddb040e4b8681a24279
MacreenSmith <macreensmith@gmail.com>
2024-01-25 13:19:52 -0500

    Created navigation links to the top of the page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import { React } from 'react'
 import logo from '../../assets/images/logo.png'
+import { NavLink, Link } from 'react-router-dom'
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/doctors',
+    display: 'Find a Doctor'
+  },
+  {
+    path: '/services',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+]
+
 const Header = () => {
-  return <header className='header flex items-center'>
-    <div className='container'>
-      <div className='flex items-center justify-between'>
-        {/* Logo Goes Here */}
-        <div>
-          <img src={logo} alt='' />
+  return (
+    <header className="header flex items-center">
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          {/* Logo Goes Here */}
+          <div>
+            <img src={logo} alt='' />
+          </div>
+
+          {/* Menu Goes Here */}
+          <div className='navigation'>
+            <ul className='menu flex items-center gap-[2.7rem]'>
+                <p className='text-textColor text-[16px] leading-7 font-[500]'>TESTING</p>
+              {
+                navLinks.map((link, index) => (
+                  <li key={index}>
+                    <NavLink to={link.path} className={navClass => navClass.isActive
+                      ? 'text-primaryColor text-[16px] leading-7 font-[600]' :
+                      'text-textColor text-[16px] leading-7 font-[500]'}>
+                      {link.display}
+                    </NavLink>
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </header>
-  
+  )
 }
 
 export default Header
--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -4,15 +4,14 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Routers from '../routes/Routers';
 const Layout = () => {
-  return (
-    <>
+  return <>
       <Header />
       <main>
         <Routers />
       </main>
       <Footer />
     </>
-  )
+  
 };
 
 export default Layout;
