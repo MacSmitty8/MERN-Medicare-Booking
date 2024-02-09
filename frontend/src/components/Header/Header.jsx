8c87f90423bee6ee8a45e07961c89188dd868c65
MacreenSmith <macreensmith@gmail.com>
2024-02-09 11:29:57 -0500

    Added hover effects to the nav links, and added a avatar with an icon

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { React } from 'react'
 import logo from '../../assets/images/logo.png'
+import userImg from '../../assets/images/avatar-icon.png'
 import { NavLink, Link } from 'react-router-dom'
 
 const navLinks = [
@@ -34,18 +35,27 @@ const Header = () => {
           {/* Menu Goes Here */}
           <div className='navigation'>
             <ul className='menu flex items-center gap-[2.7rem]'>
-                <p className='text-textColor text-[16px] leading-7 font-[500]'>TESTING</p>
               {
                 navLinks.map((link, index) => (
                   <li key={index}>
                     <NavLink to={link.path} className={navClass => navClass.isActive
                       ? 'text-primaryColor text-[16px] leading-7 font-[600]' :
-                      'text-textColor text-[16px] leading-7 font-[500]'}>
+                      'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'}>
                       {link.display}
                     </NavLink>
                   </li>
                 ))}
             </ul>
+          </div>
+          {/* nav right */}
+          <div className='flex items-center gap-4'>
+            <div>
+              <Link to='/'>
+              <figure className='w-[35px] h-[35px] rounded-full'>
+                <img src={userImg} className='w-full rounded-full' alt=""/>
+              </figure>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
