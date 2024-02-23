6379652be344867dc5931f73aae7f08819f312c1
MacreenSmith <macreensmith@gmail.com>
2024-02-23 13:36:54 -0500

    Created an About Section, and added a image displaying a doctors info on it.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,11 @@ import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
+import icon02 from '../assets/images/icon02.png'
+import icon03 from '../assets/images/icon03.png'
 import {Link} from 'react-router-dom'
 import {BsArrowRight} from 'react-icons/bs'
+import About from '../components/About/About'
 
 const Home = () => {
   return <>
@@ -77,6 +80,7 @@ const Home = () => {
       </section>
       {/* Hero Section Ends Here. */}
       <section>
+      {/* This section contains blurbs about the services that the Medicare provides. */}
         <div className='containter'>
           <div className='lg:w-[470px] mx-auto'>
             <h2 className='heading text-center'>Providing the best medical services</h2>
@@ -85,7 +89,7 @@ const Home = () => {
           </div>
 
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
-            <div className='py-[30px] px-5'>
+          <div className='py-[30px] px-5'>
               <div className='flex items-center justify-center'><img src={icon01} alt='' /> </div>
               <div className='mt-[30px]'>
                 <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
@@ -98,11 +102,39 @@ const Home = () => {
                 </Link>
               </div>
             </div>
+          <div className='py-[30px] px-5'>
+              <div className='flex items-center justify-center'><img src={icon02} alt='' /> </div>
+              <div className='mt-[30px]'>
+                <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Location</h2>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers 
+                unmatched, expert health care. From the lab to the clinc.</p>
 
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
+                mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                </Link>
+              </div>
+            </div>
+            <div className='py-[30px] px-5'>
+              <div className='flex items-center justify-center'><img src={icon03} alt='' /> </div>
+              <div className='mt-[30px]'>
+                <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Book Appointment</h2>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers 
+                unmatched, expert health care. From the lab to the clinc.</p>
+
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
+                mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </section>
-
+        {/* Here is the about section, where it'll explain what the Medicare company is about. */}
+        <section>
+          <About />
+        </section>
     </>
   </>
 }
