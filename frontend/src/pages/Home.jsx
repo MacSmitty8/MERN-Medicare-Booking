a4a64fd1bc8cacf78093eba764462b362182e0bd
MacreenSmith <macreensmith@gmail.com>
2024-02-23 12:32:22 -0500

    Added a link for the find a doctor segement, as well as an arrow.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
+import {Link} from 'react-router-dom'
+import {BsArrowRight} from 'react-icons/bs'
 
 const Home = () => {
   return <>
@@ -89,6 +91,11 @@ const Home = () => {
                 <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
                 <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers 
                 unmatched, expert health care. From the lab to the clinc.</p>
+
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
+                mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                </Link>
               </div>
             </div>
 
