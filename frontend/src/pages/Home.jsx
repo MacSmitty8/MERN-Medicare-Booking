9efc734854d71143c9c34a84aaa8957fd707baaa
MacreenSmith <macreensmith@gmail.com>
2024-02-28 14:07:21 -0500

    Added the data needed for the services section, and created the service list so that it'll render.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import icon03 from '../assets/images/icon03.png'
 import {Link} from 'react-router-dom'
 import {BsArrowRight} from 'react-icons/bs'
 import About from '../components/About/About'
+import ServiceList from '../components/Services/ServiceList'
 
 const Home = () => {
   return <>
@@ -135,6 +136,20 @@ const Home = () => {
         <section>
           <About />
         </section>
+        {/* Services Section Starts Here */}
+
+        <section>
+          <div className='container'>
+            <div className='xl:w-[470px] mx-auto'>
+              <h2 className='heading text-center'>Our Medical Services</h2>
+              <p className='text_para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+            </div>
+            <ServiceList />
+          </div>
+        </section>
+
+        {/* Services Section Starts Ends Here */}
+
     </>
   </>
 }
