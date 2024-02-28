6ae96d1b6cac0ac460e42349717f414ceba77742
MacreenSmith <macreensmith@gmail.com>
2024-02-28 13:04:41 -0500

    Added blurb text and a button with a link.

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import aboutImg from '../../assets/images/about.png'
 import aboutCardImg from '../../assets/images/about-card.png'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return <section>
@@ -12,6 +13,17 @@ const About = () => {
                     <div className='absolute z-20 bottom-4 w-[200px] md:w-[300px]'>
                         <img src={aboutCardImg} alt="" />
                     </div>
+                </div>
+                {/* The contents for the about component. */}
+                <div className='w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2'>
+                    <h2 className='heading'>Proud to be one of the nations best</h2>
+                    <p className='text_para'>
+                    30 years in a row, U.S. News & World Report has 
+                    recognized us as one of the best public 
+                    hospitals in the nation and #1 in Texas.</p>
+                    <p className='text_para mt-[30px]'>Our best is something we strive for each day, caring for
+                    our patients-not looking back at what we accomplished but towards what we can do tomorrow. Providing for the best.</p>
+                    <Link to='/'><button className='btn'>Learn More</button></Link>
                 </div>
             </div>
         </div>
