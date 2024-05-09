2e919ff25a9b56b7d177d581fbb468d6930129b0
MacreenSmith <macreensmith@gmail.com>
2024-05-09 13:16:34 -0400

    Added a span and a video icon as well as a date an time for a video call.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
+import featureImage from '../assets/images/feature-img.png'
+import videoIcon from '../assets/images/video-icon.png'
 import {Link} from 'react-router-dom'
 import {BsArrowRight} from 'react-icons/bs'
 import About from '../components/About/About'
@@ -167,6 +169,28 @@ const Home = () => {
                 use the online scheduling tool to select an appointment time.</li>
                 </ul>
                 <Link to='/'><button className='btn'>Learn More</button></Link>
+              </div>
+              {/* feature image */}
+              <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+                <img src={featureImage} className='w-3/4' alt=''/>
+
+                <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] 
+                md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:pb-[26px] rounded-[10px]'>
+                  <div className='flex items-center justify-between'>
+                    <div className='flex items-center gap-[6px] lg:gap-3'>
+                      <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
+                        Tue, 24
+                      </p>
+                      <p className='text-[10px] leading-[10px] lg:leading-5 text-headingColor font-[400]'>
+                        10:00AM
+                      </p>
+                    </div>
+                    <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center 
+                    justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                      <img src={videoIcon} alt=''/>
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
             </div>
