644eb0399942a5f97b50a82b42729d6914ab02a0
MacreenSmith <macreensmith@gmail.com>
2024-07-02 13:03:29 -0400

    Added the Doctors specialty under their name

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import starIcon from '../../assets/images/Star.png'
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
 const DoctorCard = ({doctors}) => {
     const {
         name, 
         avgRating, 
         totalRating, 
         photo, 
-        specialization, 
+        specialty, 
         totalPatients, 
         hospital, 
     } = doctors;
@@ -18,6 +20,11 @@ const DoctorCard = ({doctors}) => {
       </div>
       <h2 className='text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-headingColor font-[700]
       mt-3 lg:mt-5'>{name}</h2>
+
+      <div className='mt-2 lg:mt-4 flex items-center justify-between'>
+        <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 
+        text-[18px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>{specialty}</span>
+      </div>
     </div>
   );
 };
