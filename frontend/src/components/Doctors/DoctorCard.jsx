351e43b1c22ca6662bbedbf9601312b4d18d4fd3
MacreenSmith <macreensmith@gmail.com>
2024-07-02 12:52:20 -0400

    Made the Doctors names visible.

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -10,12 +10,14 @@ const DoctorCard = ({doctors}) => {
         totalPatients, 
         hospital, 
     } = doctors;
-    
+
   return (
     <div className='p-3 lg:p-5'>
       <div>
       <img src={photo} className='w-full' alt='' />
       </div>
+      <h2 className='text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-headingColor font-[700]
+      mt-3 lg:mt-5'>{name}</h2>
     </div>
   );
 };
