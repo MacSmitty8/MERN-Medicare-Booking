474769bf07f0bd5788f8bff196e62ba615780e24
MacreenSmith <macreensmith@gmail.com>
2025-04-02 21:38:26 -0400

    Added a FAQ List, Section, as well a Testimonial section for the website

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,13 +9,13 @@ import featureImage from '../assets/images/feature-img.png'
 import faqImg from '../assets/images/faq-img.png'
 import videoIcon from '../assets/images/video-icon.png'
 import avatarIcon from '../assets/images/avatar-icon.png'
-import {Link} from 'react-router-dom'
-import {BsArrowRight} from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+import { BsArrowRight } from 'react-icons/bs'
 import About from '../components/About/About'
 import ServiceList from '../components/Services/ServiceList'
 import DoctorList from '../components/Doctors/DoctorList'
 import FAQList from '../components/FAQ/FAQList'
-
+import Testimonial from '../components/Testimonial/Testimonial'
 const Home = () => {
   return <>
     {/* Hero Section */}
@@ -87,8 +87,8 @@ const Home = () => {
       </section>
       {/* Hero Section Ends Here. */}
       <section>
-      {/* This section contains blurbs about the services that the Medicare provides. */}
-        <div className='containter'>
+        {/* This section contains blurbs about the services that the Medicare provides. */}
+        <div className='container'>
           <div className='lg:w-[470px] mx-auto'>
             <h2 className='heading text-center'>Providing the best medical services</h2>
             <p className='text_para text-center'>
@@ -96,29 +96,29 @@ const Home = () => {
           </div>
 
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
-          <div className='py-[30px] px-5'>
+            <div className='py-[30px] px-5'>
               <div className='flex items-center justify-center'><img src={icon01} alt='' /> </div>
               <div className='mt-[30px]'>
                 <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
-                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers 
-                unmatched, expert health care. From the lab to the clinc.</p>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers
+                  unmatched, expert health care. From the lab to the clinc.</p>
 
                 <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
                 mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
-                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                  <BsArrowRight className='group-hover:text-white w-6 h-5' />
                 </Link>
               </div>
             </div>
-          <div className='py-[30px] px-5'>
+            <div className='py-[30px] px-5'>
               <div className='flex items-center justify-center'><img src={icon02} alt='' /> </div>
               <div className='mt-[30px]'>
                 <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Location</h2>
-                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers 
-                unmatched, expert health care. From the lab to the clinc.</p>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers
+                  unmatched, expert health care. From the lab to the clinc.</p>
 
                 <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
                 mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
-                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                  <BsArrowRight className='group-hover:text-white w-6 h-5' />
                 </Link>
               </div>
             </div>
@@ -126,116 +126,130 @@ const Home = () => {
               <div className='flex items-center justify-center'><img src={icon03} alt='' /> </div>
               <div className='mt-[30px]'>
                 <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Book Appointment</h2>
-                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers 
-                unmatched, expert health care. From the lab to the clinc.</p>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers
+                  unmatched, expert health care. From the lab to the clinc.</p>
 
                 <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
                 mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
-                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                  <BsArrowRight className='group-hover:text-white w-6 h-5' />
                 </Link>
               </div>
             </div>
           </div>
         </div>
       </section>
-        {/* Here is the about section, where it'll explain what the Medicare company is about. */}
-        <section>
-          <About />
-        </section>
-        {/* Services Section Starts Here */}
+      {/* Here is the about section, where it'll explain what the Medicare company is about. */}
+      <section>
+        <About />
+      </section>
+      {/* Services Section Starts Here */}
 
-        <section>
-          <div className='container'>
-            <div className='xl:w-[470px] mx-auto'>
-              <h2 className='heading text-center'>Our Medical Services</h2>
-              <p className='text_para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
-            </div>
-            <ServiceList />
+      <section>
+        <div className='container'>
+          <div className='xl:w-[470px] mx-auto'>
+            <h2 className='heading text-center'>Our Medical Services</h2>
+            <p className='text_para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
           </div>
-        </section>
+          <ServiceList />
+        </div>
+      </section>
 
-        {/* Services Section Ends Here */}
+      {/* Services Section Ends Here */}
 
 
-        {/* Features Section Starts Here */}
-          <section>
-            <div className='container'>
-            <div className='flex items-center justify-between flex-col lg:flex-row'>
-              {/* Features Content */}
-              <div className='xl:w-[670px]'>
-                <h2 className='heading'>
+      {/* Features Section Starts Here */}
+      <section>
+        <div className='container'>
+          <div className='flex items-center justify-between flex-col lg:flex-row'>
+            {/* Features Content */}
+            <div className='xl:w-[670px]'>
+              <h2 className='heading'>
                 Get Virtual Treatment <br /> anytime.
-                </h2>
-                <ul className='pl-4'>
+              </h2>
+              <ul className='pl-4'>
                 <li className='text_para'>1. Schedule the appointment directly</li>
                 <li className="text_para">2. Search for your physican here, and contact their office.</li>
                 <li className="text_para">3. View our physicians who are accepting new patients
-                use the online scheduling tool to select an appointment time.</li>
-                </ul>
-                <Link to='/'><button className='btn'>Learn More</button></Link>
-              </div>
-              {/* feature image */}
-              <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
-                <img src={featureImage} className='w-3/4' alt=''/>
+                  use the online scheduling tool to select an appointment time.</li>
+              </ul>
+              <Link to='/'><button className='btn'>Learn More</button></Link>
+            </div>
+            {/* feature image */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImage} className='w-3/4' alt='' />
 
-                <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] 
+              <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] 
                 md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:pb-[26px] rounded-[10px]'>
-                  <div className='flex items-center justify-between'>
-                    <div className='flex items-center gap-[6px] lg:gap-3'>
-                      <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
-                        Tue, 24
-                      </p>
-                      <p className='text-[10px] leading-[10px] lg:leading-5 text-headingColor font-[400]'>
-                        10:00AM
-                      </p>
-                    </div>
-                    <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center 
-                    justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
-                      <img src={videoIcon} alt=''/>
-                    </span>
+                <div className='flex items-center justify-between'>
+                  <div className='flex items-center gap-[6px] lg:gap-3'>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
+                      Tue, 24
+                    </p>
+                    <p className='text-[10px] leading-[10px] lg:leading-5 text-headingColor font-[400]'>
+                      10:00AM
+                    </p>
                   </div>
-                  <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 
+                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center 
+                    justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                    <img src={videoIcon} alt='' />
+                  </span>
+                </div>
+                <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 
                   lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full
                   '>Consultation</div>
 
-                  <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
-                    <img src={avatarIcon} alt=''/>
-                    <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>
-                      Wayne Collins
-                    </h4>
-                  </div>
+                <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                  <img src={avatarIcon} alt='' />
+                  <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>
+                    Wayne Collins
+                  </h4>
                 </div>
               </div>
             </div>
-            </div>
-          </section>
-        {/* Features Section Ends Here */}
-        {/* Doctor Section Begins */}
-        <section>
-          <div className='container'>
-            <div className='xl:w-[470px] mx-auto'>
-              <h2 className='heading text-center'>Our great doctors</h2>
-              <p className='text_para text-center'>
-                World-class care for everyone. Our health System offers unmatched,
-                expert health care.
-              </p>
-            </div>
-            <DoctorList />
           </div>
-        </section>
-        {/* Doctor Section ends here. */}
-        {/* FAQ Section starts here. */}
-        <section>
-          <div className='container'>
-            <div className='flex justify-between-gap-[50px] lg:gap-0'>
-              <div className='w-1/2 hidden md:block'><img src={faqImg} alt=''></img></div>
-              <div className='w-full md:w-1/2'>
-                <h2 className='heading'>Most questions by our beloved patients</h2>
-                <FAQList />
-              </div>
+        </div>
+      </section>
+      {/* Features Section Ends Here */}
+      {/* Doctor Section Begins */}
+      <section>
+        <div className='container'>
+          <div className='xl:w-[470px] mx-auto'>
+            <h2 className='heading text-center'>Our great doctors</h2>
+            <p className='text_para text-center'>
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+          <DoctorList />
+        </div>
+      </section>
+      {/* Doctor Section ends here. */}
+      {/* FAQ Section starts here. */}
+      <section>
+        <div className='container'>
+          <div className='flex justify-between-gap-[50px] lg:gap-0'>
+            <div className='w-1/2 hidden md:block'><img src={faqImg} alt=''></img></div>
+            <div className='w-full md:w-1/2'>
+              <h2 className='heading'>Most questions by our beloved patients</h2>
+              <FAQList />
             </div>
           </div>
-        </section>
+        </div>
+      </section>
+      {/* FAQ Section ends here. */}
+
+      {/* Testimonial section begins. */}
+      <section>
+        <div className='container'>
+          <div className='lg:w-[470px] mx-auto'>
+            <h2 className='heading text-center'>Providing the best medical services</h2>
+            <p className='text_para text-center'>
+              World-class care for everyone. Our health System offers unmatched, expert health care.</p>
+          </div>
+
+          <Testimonial />
+        </div>
+      </section>
     </>
   </>
 }
